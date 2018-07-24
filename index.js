ff6825696458c597aa31c9e05ceefb57883cf373
joe fisher <jhanajon@gmail.com>
2018-07-24 22:08:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function nowServing(line) {
 }
 }
 
+<<<<<<< HEAD
 
 function currentLine(line) {
   if(line.length <= 0) {
@@ -28,6 +29,37 @@ else {
       array.push(`${i+1}. ${line[i]}`)
       }
     return `The line is currently: ${ array.join(", ") }`
+=======
+//
+// function currentLine(line) {
+//   if(line.length > 0) {
+//     var array = []
+//     for (var i = 0; i < line.length; i++) {
+//       array.push(`${i+1}. ${line[i]}`)
+//     }
+//     return `The line is currently: ${ array.join(", ") }`
+//   } else {
+//     if(line.length <= 0) {
+//       return "The line is currently empty."
+//     }
+//   }
+// }
+
+// while (n < 3) {
+//   n++;
+// }
+
+function currentLine(line) {
+// console.log(line)
+    if(line.length <= 0) {
+      return "The line is currently empty."
+>>>>>>> 1a1f81c6bf39301d5d0e2dbbd7f932925fa4e5ac
     }
-  }
-}
+
+    else {
+      for (var i = 0; i < line.length; i++) {
+        line.push(`${i+1}. ${line[i]}`)
+      }
+        return `The line is currently: ${ line.join(", ")  }`
+        }
+      }
